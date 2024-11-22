9ea7ba32ab32957e55ef38713076da8596c4218e
itsjonlol <jonathansuherman@gmail.com>
2024-11-22 11:28:34 +0800

    added config

---
+++ src/main/java/sg/edu/nus/iss/vttp5a_ssf_day15l/util/Util.java
@@ -0,0 +1,8 @@
+package sg.edu.nus.iss.vttp5a_ssf_day15l.util;
+
+public class Util {
+
+    public static final String template01 = "template01";
+    public static final String template02 = "template02";
+    
+}
